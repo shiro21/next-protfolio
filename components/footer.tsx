495a44c48734230378f261ca831ba21000d0db73
junhyeok <junhyeok403@naver.com>
2023-01-25 15:54:38 +0900

    Fix : target TEST

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,10 +11,10 @@ export default function Footer() {
                         </svg>
                         <span className="ml-3 text-xl">JunHyeok Portfolio</span>
                     </Link>
-                    {/* <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2023 Tailblocks —
+                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2023 Tailblocks —
                         <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
-                    </p> */}
-                    {/* <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    </p>
+                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a className="text-gray-500">
                             <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -37,7 +37,7 @@ export default function Footer() {
                                 <circle cx="4" cy="4" r="2" stroke="none"></circle>
                             </svg>
                         </a>
-                    </span> */}
+                    </span>
                 </div>
             </div>
         </footer>
